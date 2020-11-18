bafce50d0f14e7f4921300e4e7e012c9a031fd86
toko202011 <toko202011@gmail.com>
2020-11-18 14:01:44 +0000

    user signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "ユーザを登録しました。"
+      redirect_to '/'
+    else
+      flash.now[:danger] = "ユーザーを登録できませんでした。"
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  get 'toppages/index'
+  get 'users/index'
+  get 'users/show'
+  get 'users/new'
+  get 'users/create'
+  # get 'toppages/index'  root to: 'tasks#index'に変更
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'tasks#index'
   
+  get 'signup', to: 'users#new'
+
   resources :tasks
+  resources :users, only: [:index, :new, :create]
 end
