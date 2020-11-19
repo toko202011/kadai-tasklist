b0ad5889845e5e255b5088c495846839f594e8ca
toko202011 <toko202011@gmail.com>
2020-11-19 00:43:54 +0000

    Update user login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,8 +1,8 @@
-module ApplicationHelper
-  def current user
+module SessionsHelper
+  def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
-
+  
   def logged_in?
     !!current_user
   end
